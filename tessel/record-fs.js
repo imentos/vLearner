30deb52499c04ab1cacdf9804ea3a1f4236991ba
Ray Kuo <ray.kuo@sap.com>
2015-12-04 11:50:37 -0800

    remove unused code

--- tessel/record-fs.js
+++ tessel/record-fs.js
@@ -4,50 +4,6 @@ var currentId = null;
 var chunks = [];
 var fs = require('fs');
 
-// audio.startRecording(function(err) {
-//     // In one second
-//     setTimeout(function() {
-//         // Stop recording
-//         audio.stopRecording(function(err) {
-
-//             console.log('Writing...');
-//             fs.writeFile('someFile.txt', 'Hey Tessel SDCard!', function(err) {
-//                 console.log("Wrote to a file");
-
-//                 audio.setOutput('headphones', function(err) {
-
-//                     // Open a file
-//                     console.log('Write complete. Reading...');
-//                     fs.readFile('someFile.txt', function(err, data) {
-//                         console.log('Read someFile.txt:\n');
-
-//                         // Play the file
-//                         // audio.setVolume(1.0, function(err) {
-//                         audio.play(data, function(err) {
-//                             chunks = [];
-//                             currentId = null;
-//                         });
-//                     });
-
-//                 });
-
-
-//             });
-//         });
-
-//     });
-// });
-
-
-
-// audio.on('ready', function() {
-//     console.log('Ready to record audio');
-//     audio.on('data', function(data) {
-//         chunks.push(data);
-//     });
-// });
-
-
 audio.on('ready', function() {
     // Start recording data for a second into a file
     audio.setInput('mic', function(err) {
